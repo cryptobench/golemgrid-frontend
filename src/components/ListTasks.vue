5152f3aefb96e88da6c1a612888cc0406317d768
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-10-08 15:15:27 +0200

    Fix typo

--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -10,7 +10,7 @@
             class="inline-flex items-center mt-1 mb-2 px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-golemblue hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <PlusIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-            Start new teask
+            Create Task
           </button>
         </div>
         <div class="bg-white shadow overflow-hidden sm:rounded-md">
