38da29a0cba951f14fc906be999c009fdcc29ae9
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-12-19 00:23:00 +0100

    Change button text

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -133,7 +133,7 @@
                     type="submit"
                     class="w-full bg-golemblue border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
                   >
-                    Submit
+                    Upload Scene
                   </button>
                 </div>
               </div>
